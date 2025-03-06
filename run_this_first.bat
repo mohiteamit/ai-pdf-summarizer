81907c0a08b875e3a606ec6fd7279a5e4e0fc075
Amit Mohite <amit.mohite@outlook.com>
2025-03-06 22:42:46 +0530

    some clean up

--- run_this_first.bat
+++ run_this_first.bat
@@ -1,2 +1,11 @@
+@echo off
+echo Loading environment variables...
+for /f "tokens=1,2 delims==" %%a in ('type .env') do (
+    set %%a=%%b
+)
+
+echo Activating virtual environment...
+call %VENV_PATH%
+
 set PYTHONPATH=%CD%
 streamlit run app/main.py
